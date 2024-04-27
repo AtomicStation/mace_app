aad310f7a5de4a41edeaf06836fc430491462814
Tom Bukowski <tbukowski88@gmail.com>
2024-04-27 12:45:48 -0600

    Improved rep counting logic, can handle clubs and maces

--- model_predict.py
+++ model_predict.py
@@ -7,116 +7,176 @@ from custom_model import *
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
-# Are we demoing the clubbell model?
-demo = False
+PROJECT = 'Clubbell_nohold_noidle'
+MOVEMENTS = ['swing', 'open']
 
-actions = []
-
-PROJECT = 'Clubbell'
-MOVEMENTS = ['swing', 'open', 'hold', 'idle']
+# PROJECT = 'Mace_Demo'
+# MOVEMENTS = ['swing', 'hold', 'idle']
 
 # New detection variables
-sequence = [] # collect 30 frames to make a prediction
+# list of the last keypoints
+sequence = []
+
+# list of predictions
 predictions = []
-action_seq = []
-swing_seq = ['swing', 'open', 'hold']
+
+# predicted action
+pred_action = ''
+
+# predicted probability
+pred_prob = 0
+
+# rep counter
 counter = 0
-current_class = 'idle'
-threshold = 0.5 # confidence
+
+# Confidence threshold that the action is what we think it is
+threshold = 0.8
+
+# Variable to check if we changed actions
+check_action = ''
+
+# Debug stuff
+confirmed = []
+test_counter = 0
+
+# TO DO: make functions have this font already built in
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Sort the movement actions for when we build the model
+sorted_actions = sorted(MOVEMENTS)
+
+# Model requires the actions to be a numpy array
+actions = np.array(sorted_actions)
+
+# Models have inconsistent reset action names 'open' vs 'hold'
+not_swing_index = np.where(actions != 'swing')[0]
+not_swing_actions = actions[not_swing_index]
+
+# If its a 2 category model, reset action is the not swing action
+if actions.size == 2:
+    reset_action = not_swing_actions[0]
+else:
+    # For 3 category models, the other action is 'idle'
+    reset_index = np.where(not_swing_actions != 'idle')[0]
+    reset_action = not_swing_actions[reset_index][0]
+
+# Build the model using the actions array
+model = build_model(actions)
+
+# Load the projects pre-trained weights
+model.load_weights(PROJECT + '_weights.h5')
+
+# Check if this is a live demo or using previously recorded video
+demo = True
 
 if demo:
-    actions = np.array(['hold', 'swing'])
-    model = build_model(actions)
-    model.load_weights('clubbell_weights.h5')
+    # OpenCV uses webcam (value 0)
     cap = cv2.VideoCapture(0)
 
 else:
-    sorted_actions = sorted(MOVEMENTS)
-    actions = np.array(sorted_actions)
-    model = build_model(actions)
-    model.load_weights(PROJECT + '_weights_300.h5')
-    cap = cv2.VideoCapture(0)
+    # OpenCV needs to find the video
+    video_name = PROJECT.lower()
+    video_path = 'videos/' + video_name + '.mp4'
+    cap = cv2.VideoCapture(video_path)
 
-# cap = cv2.VideoCapture("videos/MaceCV/IMG_0020.MOV")
-
-#cap = cv2.VideoCapture("videos/webcam_test.mp4")
-
-# Set mediapipe model 
+# Start using the MediaPipe model
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # Give countdown timer before starting to capture frames for detection
+    for i in range(100):
+        # Update feed
+        ret, image = cap.read()
+
+        # "GET READY STARTING SOON"
+        new_text = "GET READY STARTING SOON"
+        text_width, text_height = get_size(new_text, font, 1, 4)
+        textX, textY = get_center(text_width, text_height, cap)
+        cv2.putText(image, new_text, (textX, textY), font, 1, (0,255, 0), 4, cv2.LINE_AA)
+
+        # Progress bar
+        image = progress_bar(cap, image, i/100)
+
+        # Show modified frame to screen
+        cv2.imshow('OpenCV Feed', image)
+        cv2.waitKey(30)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    
+    # Start capturing frames for detection
     while cap.isOpened():
 
-        # Read feed
+        # Get current frame and whether or not there is a current frame (ret)
         ret, frame = cap.read()
+
+        # If ret is False, no longer capturing frames and need to break
+        if not ret:
+            break
 
         # Make detections
         image, results = mediapipe_detection(frame, holistic)
-        # print(results)
         
         # Draw landmarks
         draw_pose_landmarks(image, results)
         
-        # 2. Prediction logic
+        # Extract the keypoints from the current frame
         keypoints = extract_keypoints(results)
+
+        # Add these to the sequence list
         sequence.append(keypoints)
+
+        # take the last 30 sequences and keep track of them
         last_sequence = sequence[-30:]
-        
+
+
+        # When we have 30 frames of data, we can make a prediction
         if len(last_sequence) == 30:
+            # get prediction, but first make sure the shape of the array is correct so expand
             res = model.predict(np.expand_dims(last_sequence, axis=0), verbose=0)[0]
-            predictions.append(np.argmax(res))
-            body_language_class = actions[np.argmax(res)]
-            body_language_prob = res[np.argmax(res)]
 
-            # # Counter logic
-            # if body_language_class == 'swing' and body_language_prob >= 0.7:
-            #     current_stage = 'swing'
-            # elif current_stage == 'swing' and body_language_class == 'hold' and body_language_prob >= 0.7:
-            #     current_stage = 'hold'
-            #     counter += 1
+            # The prediction will be an array of probability values, we want the highest probability index
+            index_res = np.argmax(res)
 
-            if body_language_prob >= 0.9 and current_class != body_language_class:
-                # update current_class
-                current_class = body_language_class
-                action_seq.append(body_language_class.lower())
-                print('High confidence this is: {}'.format(body_language_class))
-
-            last_actions = action_seq[-3:]
-            if len(last_actions) == 3 and last_actions == swing_seq:
-                counter += 1
-                print('Counter: {}'.format(counter))
-
-
-
-
-            # # Status Box
-            # cv2.rectangle(image, (0,0), (250, 60), (245, 117, 16), cv2.FILLED)
+            # add this prediction to a list
+            predictions.append(index_res)
             
-            
-            # # Display Class
-            # cv2.putText(image, 'CLASS'
-            #             , (95,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-            # cv2.putText(image, body_language_class.split(' ')[0]
-            #             , (90,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-            
-            # # Display Probability
-            # cv2.putText(image, 'PROB'
-            #             , (15,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-            # cv2.putText(image, str(round(body_language_prob,2))
-            #             , (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-            
-            # # Display Count
-            # cv2.putText(image, 'Count'
-            #             , (200,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-            # cv2.putText(image, str(counter)
-            #             , (200,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+            # Use this index to get the name of the action
+            pred_action = actions[index_res]
 
-            image = prob_viz(res, actions, image)
+            # Use the index to get the probability of the action
+            pred_prob = res[index_res]
+
+            # if the last -N predictions equal the current prediction, there's a high confidence in that prediction
+            if np.unique(predictions[-10:])[0] == index_res:
+
+                # Check the probability versus our confidence threshold
+                if pred_prob > threshold:
+                    # if the previous action was swing and current action is the reset_action
+                    if check_action == 'swing' and pred_action == reset_action:
+                        # count the rep
+                        counter += 1
+                    
+                    # after the check, the current action becomes the new check action for next time
+                    check_action = pred_action
+
+                    # Debugging and tracking the sequence of confirmed actions
+                    if len(confirmed) > 0:
+                        if pred_action != confirmed[-1]:
+                            confirmed.append(actions[index_res])
+                    else:
+                        confirmed.append(actions[index_res])
+
+            # image = prob_viz(res, actions, image)
+
+            # Display the counter to the screen
+            image = display_count(image, counter)
     
         # Show to screen
         cv2.imshow('OpenCV Feed', image)
-        cv2.waitKey(30)
+        cv2.waitKey(20)
 
         # Break gracefully
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        if cv2.waitKey(20) & 0xFF == ord('q'):
             break
+
     cap.release()
     cv2.destroyAllWindows()
